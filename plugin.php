847a5dbc81a9d6b0ff9cf2e338f48a036623248c
kansomaryam <maryam@happywp.co>
2024-10-20 23:14:41 +0300

    Initial commit for new plugin

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * sample plugin
+ *
+ * @package     blogger
+ * @author      Maryam K
+ * @copyright   2024 Maryam K
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Finance Stats Dashboard
+ * Plugin URI:  http://blogger.local/sample-plugin
+ * Description: This plugin allows users to display key financial metrics, statistics, and growth.
+ * Version:     1.0.0
+ * Author:      Maryam K
+ * Author URI:  https://example.com
+ * Text Domain: plugin-slug
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+*/
+
+ namespace blogger;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lenovo/sample-plugin',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '193460350e40cda3f5bea7fa3628222daa631e73',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lenovo/sample-plugin' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '193460350e40cda3f5bea7fa3628222daa631e73',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
